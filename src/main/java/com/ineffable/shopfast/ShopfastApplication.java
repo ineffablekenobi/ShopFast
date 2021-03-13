ef353ac5f0f4be6b3df397c39210aa46da0d087d
swapnil <swapnilsharma98@live.com>
2021-03-14 02:22:16 +0600

    GG

---
+++ src/main/java/com/ineffable/shopfast/ShopfastApplication.java
@@ -0,0 +1,13 @@
+package com.ineffable.shopfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopfastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopfastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ineffable/shopfast/ShopfastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ineffable.shopfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopfastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
